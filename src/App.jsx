d9960c211f5c10a4700dac98caf143794f36e792
kingsley etukudo <kingsleyj90@gmail.com>
2025-01-16 21:55:20 +0100

    just loading the repo

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <h1 className="text-9xl text-center">Plexity Rate</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
